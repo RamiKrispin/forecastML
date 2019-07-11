abceb63c10f919028bf711ea7be3e142bfd529df
RamiKrispin <ramkrisp@umich.edu>
2019-07-10 23:55:14 -0700

    updated the residauls plot

--- R/plots.R
+++ R/plots.R
@@ -2,7 +2,13 @@
 #' @export
 #' @param model A trainML object
 
-plot_res <- function(model){
+plot_res <- function(model, na.rm = FALSE){
+
+  if(base::class(model) != "trainML"){
+    stop("The input model is not a 'trainML' object")
+  }
+
+
   p1 <- plotly::plot_ly(x = model$series[[model$parameters$index]],
                         y = model$series[[model$parameters$y]],
                         type = "scatter", mode = "lines") %>%
@@ -12,8 +18,15 @@ plot_res <- function(model){
   p2 <- plotly::plot_ly(data = model$residuals, x = ~ index, y = ~ residuals, type = "scatter", mode = "lines") %>%
     plotly::layout(yaxis = list(title = "Residuals"),
                    xaxis = list(title = "Index"))
-  p3 <- forecastML::tsACF(model$residuals)
 
+  if(base::any(base::is.na(model$residuals)) && na.rm == FALSE){
+    stop("The model residuals has missing values, please either check the residuals or set na.rm = TRUE")
+  }
+  p3 <- forecastML::tsACF(model$residuals, na.rm = na.rm)
 
-  plotly::subplot(p1,p2, p3$residuals$plot, nrows = 3)
+  p4 <- plotly::plot_ly(x = model$residuals$residuals, type = "histogram")
+
+  plotly::subplot(p1,p2, plotly::subplot(p3$residuals$plot, p4, nrows = 1 ), nrows = 3, titleY = TRUE, titleX = TRUE)
 }
+
+
